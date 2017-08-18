1d43a7399572b322fe481a471c25457eb167a1dd
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-18 15:35:45 -0500

    Created check for user log in and if not logged them in

--- views/users/login.php
+++ views/users/login.php
@@ -1,50 +1,19 @@
-<div class="container">
+<?php
+require_once '../utils/Auth.php';
+session_start();
+$data['state'] = false;
 
-    <section id="login">
+if (Auth::check()) {
+    $data['state'] = 'error already logged in';
+    print(json_encode($data));
+    die();
+}
 
-        <div class="row">
+    $login = Auth::attempt($_POST['username'],$_POST['password']);
 
-            <h1 class="section-title">Login To OooLister</h1>
+    if ($login) {
+        $person = Model::find($_SESSION['LOGGED_IN_ID']);
+        print(json_encode($person));
+    }
 
-            <?php if (isset($_SESSION['ERROR_MESSAGE'])) : ?>
-                <div class="alert alert-danger">
-                    <p class="error"><?= $_SESSION['ERROR_MESSAGE']; ?></p>
-                </div>
-                <?php unset($_SESSION['ERROR_MESSAGE']); ?>
-            <?php endif; ?>
-            <?php if (isset($_SESSION['SUCCESS_MESSAGE'])) : ?>
-                <div class="alert alert-success">
-                    <p class="success"><?= $_SESSION['SUCCESS_MESSAGE']; ?></p>
-                </div>
-                <?php unset($_SESSION['SUCCESS_MESSAGE']); ?>
-            <?php endif; ?>
-
-            <div class="col-md-6 col-md-offset-3">
-
-                <p>Login with your email/username and password</p>
-
-                <form method="POST" action="" data-validation data-required-message="This field is required">
-
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="email_user" name="email_user" placeholder="Email or Username" data-required>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </div>
-                        <div class="col-sm-6 text-right">
-                            <a href="/signup" class="btn btn-success">Go To Signup</a>
-                        </div>
-                    </div>
-                </form>
-
-            </div>
-
-        </div>
-
-    </section>
-
-</div>
+ ?>
