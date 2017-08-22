400d81aa69ba504a439a082ec32cb0c2072db2b4
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-22 09:47:26 -0500

    changed login to have session start and testing logout file

--- public/logout.php
+++ public/logout.php
@@ -1,9 +1,11 @@
 <?php
+
         session_unset();
         session_destroy();
         session_regenerate_id();
         session_start();
 
-        print(json_encode('Logout succesful'));
+
+        print(json_encode("true"));
 
 ?>
